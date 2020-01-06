9ea06077a71f2b44a49a5c021f462e79499fa863
2magpies <daphnemintz@yahoo.com>
2020-01-06 14:41:14 -0500

    Updated question-answer array

--- script.js
+++ script.js
@@ -6,14 +6,23 @@ console.log('warm and fuzzy feelings');
 //GRAB A QUESTION FROM ARRAY
 
 // create question array
+// const questionList = [
+//   'Where was Duke Kahanamoku born?',
+//   'Who was the first professional female surfer?',
+//   'What is the minimum length for a longboard?'
+// ];
+
 const questionList = [
-  'Where was Duke Kahanamoku born?',
-  'Who was the first professional female surfer?',
-  'What is the minimum length for a longboard?'
+  {
+    question: 'Where was Duke Kahanamoku born?',
+    answerA: 'Honolu, HI',
+    answerB: 'Los Angles, CA',
+    answerC: 'Istanbul, Turkey'
+  }
 ];
 
 //select Let's Play button
-const letsPlayButton = document.querySelector('letsPlay');
+const letsPlayButton = document.querySelector('.letsPlay');
 console.log(letsPlayButton);
 
 //add event listener to lets play button
@@ -21,14 +30,15 @@ letsPlayButton.addEventListener('click', handleLetsPlayButton);
 
 //create callback function for lets play button
 function handleLetsPlayButton() {
+  //add question counter to track question
+  var currentQuestion = 0;
   //return a question
-  document.querySelector('.question').innerHTML = questionList[0];
+  document.querySelector('.question').innerHTML =
+    questionList[currentQuestion].question;
+
+  //create element button with for loop
+
+  //populate button with innerHTML for answerA
 }
-
-// select question
-
-//add event listener to element
-
-//add callback function
 
 //GRAB LIST OF ANSWERS FOR QUESTION
