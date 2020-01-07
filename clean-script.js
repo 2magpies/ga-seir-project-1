cea983e935b5fe332835e54a1f2ecbdbd896421e
2magpies <daphnemintz@yahoo.com>
2020-01-07 14:56:07 -0500

    Created new js file and updated README

---
+++ clean-script.js
@@ -0,0 +1,15 @@
+// give myself that warm and fuzzy console log message
+
+// create an array of 3 question objects that include a question, 3 answers, a image source for a hint, and the correct answer
+
+//add an object to identify a question when it is invoked
+
+//add function to define the behaviour of the Let's Play button in the html
+
+//get the first question in the questions array
+
+//add function to let's play button to create 3 answer buttons
+
+//insert the possible answers a, b, and c into the answer buttons
+
+//add function to check answer when user 
